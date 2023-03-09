412b81be6922770148c23a76374ed689d3dc28f4
NORZACO AS <norzaco@gmail.com>
2023-03-09 10:42:33 +0100

    Representation headers

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 // router.get('/', function(req, res, next) {
+//   console.log(req.query)
 //   res.render('index', { title: 'Express' });
 // });
 
 
-
-
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log(req.query)
-  res.render('index', { title: 'Express' });
+  res.send(`<?xml version="1.0" encoding="UTF-8"?> <data> some data </data>`);
 });
-
-
-
 
 
 module.exports = router;
